20821a5b8d235332d2009e8a7fe095dfc17fb125
GVMartin23 <MartinGV19@gcc.edu>
2024-02-12 12:48:55 -0500

    updated key

--- LabItEmailer/emails.py
+++ LabItEmailer/emails.py
@@ -4,7 +4,7 @@ import smtplib
 
 def email_user():
 
-    cred = credentials.Certificate("labit-b36bf-firebase-adminsdk-ntbal-cee91c0aa7.json")
+    cred = credentials.Certificate("labit-b36bf-firebase-adminsdk-ntbal-e029442de8.json")
     initialize_app(cred, {"databaseURL": "https://labit-b36bf-default-rtdb.firebaseio.com"})
 
     db = firestore.client()
